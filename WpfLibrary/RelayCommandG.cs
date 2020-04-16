345c14ac0fab61e64e3a21d8bebad426d091ed89
Nathan2503 <62427033+Nathan2503@users.noreply.github.com>
2020-04-16 12:30:32 +0200

    Add files via upload

---
+++ WpfLibrary/RelayCommandG.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfLibrary
+{
+    public class RelayCommandG<T> : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+        private Func<bool> _CanExecute;
+        private Action<T> _Execute;
+        public RelayCommandG(Action<T> execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException(nameof(execute));
+            }
+            this._CanExecute = canExecute;
+            this._Execute = execute;
+        } 
+        public RelayCommandG(Action<T> execute) : this(execute, null)
+        {
+
+        }
+        public bool CanExecute(object parameter)
+        {
+            if (this._CanExecute == null)
+            {
+                return true;
+            }
+            return this._CanExecute.Invoke();
+        }
+
+        public void Execute(object parameter)
+        {
+            this._Execute.Invoke((T)parameter);
+        }
+
+    }
+}
+
